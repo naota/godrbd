430be4dab2d7181ff8ae8766f34b3d9d75e5a14a
Naohiro Aota <naota@elisp.net>
2016-03-17 15:11:06 +0900

    Resouce: Add resouce implementation and its test

---
+++ godrbd_test.go
@@ -0,0 +1,328 @@
+package drbd
+
+import (
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+)
+
+func TestResource(t *testing.T) {
+	foores, err := NewResource("foo")
+	if err != nil {
+		t.Fatal("cannot create resource foo", err)
+	}
+	if foores.Name() != "foo" {
+		foores.Down()
+		t.Fatal("foo: name does not match")
+	}
+
+	barres, err := NewResource("bar")
+	if err != nil {
+		foores.Down()
+		barres.Down()
+		t.Fatal("cannout create resource bar", err)
+	}
+	if barres.Name() != "bar" {
+		foores.Down()
+		barres.Down()
+		t.Fatal("bar: name does not match")
+	}
+
+	reses, err := ListResources()
+	if err != nil {
+		foores.Down()
+		barres.Down()
+		t.Fatal("cannot list resources")
+	}
+	if len(reses) != 2 || reses[0] != "foo" || reses[1] != "bar" {
+		foores.Down()
+		barres.Down()
+		t.Fatal("resource list not match")
+	}
+
+	err = foores.Delete()
+	if err != nil {
+		foores.Down()
+		barres.Down()
+		t.Fatal("failed to delete foo")
+	}
+	err = barres.Delete()
+	if err != nil {
+		foores.Down()
+		barres.Down()
+		t.Fatal("failed to delete bar")
+	}
+
+	reses, err = ListResources()
+	if err != nil {
+		t.Fatal("cannot list resources")
+	}
+	if len(reses) != 0 {
+		t.Fatal("resource list should be empty")
+	}
+}
+
+func TestMinor(t *testing.T) {
+	foores := createMinor(t, "foo", 0, 0)
+
+	err := foores.DeleteMinor()
+
+	_, err = os.Stat("/dev/drbd0")
+	if err == nil {
+		foores.Down()
+		t.Fatal("/dev/drbd0 not deleted")
+	}
+
+	foores.Down()
+}
+
+func createMinor(t *testing.T, name string, minor, vol int) *Resource {
+	res, err := NewResource(name)
+	if err != nil {
+		t.Fatalf("Failed to create %s %s", name, err)
+	}
+
+	dev := fmt.Sprintf("/dev/drbd%d", minor)
+
+	_, err = os.Stat(dev)
+	if err == nil {
+		res.Down()
+		t.Fatal(dev, " exists")
+	}
+
+	err = res.CreateMinor(minor, vol)
+	if err != nil {
+		res.Down()
+		t.Fatal("Failed to create minor", err)
+	}
+
+	_, err = os.Stat(dev)
+	if err != nil {
+		res.Down()
+		t.Fatal(dev, " not created")
+	}
+
+	return res
+}
+
+func TestAttach(t *testing.T) {
+	drbdDev := "/dev/drbd0"
+	dataImg := "test-attach-data.img"
+	metaImg := "test-attach-meta.img"
+	size := int64(16) * 1024 * 1024 * 1024
+	err := withResource(t, "foo", 0, 0, dataImg, size, metaImg, size,
+		func(foores *Resource, dataDev, metaDev string) error {
+			err := foores.SetPrimary(true)
+			if err != nil {
+				foores.Down()
+				return err
+			}
+
+			dlen := 1024
+			data := make([]byte, dlen)
+			_, err = rand.Read(data)
+			if err != nil {
+				foores.Down()
+				return err
+			}
+
+			file, err := os.OpenFile(drbdDev, os.O_WRONLY, 0600)
+			if err != nil {
+				foores.Down()
+				return err
+			}
+
+			_, err = file.Write(data)
+			if err != nil {
+				foores.Down()
+				return err
+			}
+			file.Sync()
+			file.Close()
+
+			// cmp result
+			file2, err := os.Open(dataImg)
+			if err != nil {
+				foores.Down()
+				return err
+			}
+
+			data2 := make([]byte, dlen)
+			_, err = file2.Read(data2)
+			if err != nil {
+				file2.Close()
+				foores.Down()
+				return err
+			}
+			file2.Close()
+
+			for i := 0; i < dlen; i++ {
+				if data[i] != data2[i] {
+					foores.Down()
+					return errors.New(fmt.Sprintf(
+						"data mismatch at %d %d <-> %d",
+						i, data[i], data2[i]))
+				}
+			}
+
+			err = foores.SetSecondary()
+			if err != nil {
+				foores.Down()
+				return err
+			}
+
+			err = foores.Detach()
+			if err != nil {
+				foores.Down()
+				return err
+			}
+
+			return nil
+		})
+	if err != nil {
+		t.Fatal("TestAttach failed", err)
+	}
+
+}
+
+func withLoop(image string, size int64, f func(dev string) error) error {
+	// check existence
+	_, err := os.Stat(image)
+	if err == nil {
+		return errors.New(fmt.Sprintf("%s exists", image))
+	}
+
+	// create image
+	_, err = os.Create(image)
+	if err != nil {
+		return err
+	}
+
+	err = os.Truncate(image, size)
+	if err != nil {
+		return err
+	}
+
+realloc:
+	out, err := exec.Command("losetup", "-f").CombinedOutput()
+	if err != nil {
+		os.Remove(image)
+		return errors.New(string(out))
+	}
+	device := string(out[0 : len(out)-1])
+
+	// setup loop dev
+	out, err = exec.Command("losetup", device, image).CombinedOutput()
+	if err != nil {
+		busystr := "failed to set up loop device: Device or resource busy\n"
+		if string(out)[len(out)-len(busystr):len(out)] == busystr {
+			// retry
+			goto realloc
+		}
+		os.Remove(image)
+		return errors.New(string(out))
+	}
+
+	err = f(device)
+
+	// tear down loop dev and image
+	// FIXME: error handling
+	exec.Command("losetup", "-d", device).Run()
+	os.Remove(image)
+	return err
+}
+
+func TestConnect(t *testing.T) {
+	dataImgFoo := "test-data-foo.img"
+	metaImgFoo := "test-meta-foo.img"
+	size := int64(16) * 1024 * 1024 * 1024
+	dataImgBar := "test-data-bar.img"
+	metaImgBar := "test-meta-bar.img"
+
+	err := withResource(t, "foo", 0, 0, dataImgFoo, size, metaImgFoo, size,
+		func(foores *Resource, dataDevFoo, metaDevFoo string) error {
+			return withResource(t, "bar", 1, 0, dataImgBar, size, metaImgBar, size,
+				func(barres *Resource, dataDevBar, metaDevBar string) error {
+					err := foores.SetPrimary(true)
+					if err != nil {
+						foores.Down()
+						barres.Down()
+						return err
+					}
+
+					fooport := "ipv4:127.0.0.1:7788"
+					barport := "ipv4:127.0.0.1:7789"
+					err = foores.Connect(fooport, barport)
+					if err != nil {
+						foores.Down()
+						barres.Down()
+						return err
+					}
+					err = barres.Connect(barport, fooport)
+					if err != nil {
+						foores.Down()
+						barres.Down()
+						return err
+					}
+
+					time.Sleep(1 * time.Second)
+
+					err = foores.Disconnect(fooport, barport)
+					if err != nil {
+						foores.Down()
+						barres.Down()
+						return err
+					}
+					err = barres.Disconnect(barport, fooport)
+					if err != nil {
+						foores.Down()
+						barres.Down()
+						return err
+					}
+
+					return nil
+				})
+		})
+	if err != nil {
+		t.Fatal("TestConnect failed", err)
+	}
+
+}
+
+func withResource(t *testing.T,
+	name string, minor, vol int,
+	dataImg string, dataSize int64, metaImg string, metaSize int64,
+	f func(res *Resource, ddev, mdev string) error) error {
+
+	res := createMinor(t, name, minor, vol)
+
+	return withLoop(dataImg, dataSize,
+		func(dataDev string) error {
+			return withLoop(metaImg, metaSize,
+				func(metaDev string) error {
+					err := res.CreateMetaDev(metaDev)
+					if err != nil {
+						return err
+					}
+
+					err = res.Attach(dataDev, "--on-io-error=detach")
+					if err != nil {
+						res.Down()
+						return err
+					}
+
+					err = f(res, dataDev, metaDev)
+					if err != nil {
+						res.Down()
+						return err
+					}
+
+					res.Down()
+					return nil
+				})
+		})
+}
---
+++ resource.go
@@ -0,0 +1,212 @@
+package drbd
+
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+	"text/scanner"
+)
+
+type Resource struct {
+	name    string
+	metadev string
+	minor   int
+	volume  int
+}
+
+func (r *Resource) Name() string {
+	return r.name
+}
+
+func (r *Resource) Delete() error {
+	err := r.DeleteMinor()
+	if err != nil {
+		return err
+	}
+
+	return exec.Command("drbdsetup", "del-resource", r.name).Run()
+}
+
+func (r *Resource) DeleteMinor() error {
+	if r.minor < 0 {
+		return nil
+	}
+	return exec.Command("drbdsetup", "del-minor", fmt.Sprintf("%d", r.minor)).Run()
+}
+
+func (r *Resource) CreateMinor(minor int, vol int) error {
+	err := exec.Command("drbdsetup", "new-minor", r.name,
+		fmt.Sprintf("%d", minor), fmt.Sprintf("%d", vol)).Run()
+	if err != nil {
+		return err
+	}
+
+	r.minor = minor
+	r.volume = vol
+
+	return nil
+}
+
+func (r *Resource) MinorDev() string {
+	if r.minor < 0 {
+		panic("minor not yet setuped")
+	}
+	return fmt.Sprintf("/dev/drbd%d", r.minor)
+}
+
+func (r *Resource) Scan() error {
+	out, err := exec.Command("drbdsetup", "show").CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	var s scanner.Scanner
+	s.Init(strings.NewReader(string(out)))
+	var tok rune
+	for tok != scanner.EOF {
+		tok = s.Scan()
+		switch s.TokenText() {
+		case "volume", "minor":
+			tok = s.Scan()
+			if tok == scanner.EOF {
+				return errors.New("drbdsetup show: scanning failure")
+			}
+
+			x, err := strconv.Atoi(s.TokenText())
+			if err != nil {
+				return err
+			}
+
+			if s.TokenText() == "volume" {
+				r.volume = x
+			} else {
+				r.minor = x
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func (r *Resource) Attach(datadev string, args ...string) error {
+	err := ApplyActivityLog(r.metadev)
+	if err != nil {
+		return err
+	}
+
+	// FIXME: flexible
+	args = append([]string{"attach", fmt.Sprintf("%d", r.minor),
+		datadev, r.metadev, "flexible"}, args...)
+	err = errorOut("drbdsetup attach", "drbdsetup", args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Resource) CreateMetaDev(metadev string) error {
+	err := errorOut("drbdmeta create-md",
+		"drbdmeta", "--force", r.MinorDev(),
+		"v08", metadev, "flex-external", "create-md")
+	if err != nil {
+		return err
+	}
+
+	r.metadev = metadev
+	return nil
+}
+
+func (r *Resource) Connect(local, remote string) error {
+	return errorOut("drbdsetup connect",
+		"drbdsetup", "connect", r.name, local, remote,
+		"--protocol", "C", "--after-sb-0pri", "discard-zero-changes")
+}
+
+func (r *Resource) Disconnect(local, remote string) error {
+	return errorOut("drbdsetup disconnect",
+		"drbdsetup", "disconnect", local, remote)
+}
+
+func (r *Resource) Detach() error {
+	return errorOut("drbdsetup detach", "drbdsetup", "detach", fmt.Sprintf("%d", r.minor))
+}
+
+func (r *Resource) Down() error {
+	return errorOut("drbdsetup down", "drbdsetup", "down", r.name)
+}
+
+func (r *Resource) SetPrimary(force bool) error {
+	args := []string{"primary", fmt.Sprintf("%d", r.minor)}
+	if force {
+		args = append(args, "--force")
+	}
+
+	return errorOut("drbdsetup primary", "drbdsetup", args...)
+}
+
+func (r *Resource) SetSecondary() error {
+	return errorOut("drbdsetup secondary",
+		"drbdsetup", "secondary", fmt.Sprintf("%d", r.minor))
+}
+
+func errorOut(prefix, cmd string, args ...string) error {
+	out, err := exec.Command(cmd, args...).CombinedOutput()
+	if err != nil {
+		return errors.New(fmt.Sprintf("%s: %s\n%s", prefix, err, out))
+	}
+	return nil
+}
+
+// FIXME: skel function
+func ApplyActivityLog(metadev string) error {
+	return nil
+}
+
+func NewResource(name string) (*Resource, error) {
+	r := new(Resource)
+	r.name = name
+	r.metadev = ""
+	r.minor = -1
+	r.volume = -1
+
+	err := exec.Command("drbdsetup", "new-resource", name).Run()
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.Scan()
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func ListResources() ([]string, error) {
+	out, err := exec.Command("drbdsetup", "show").CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+
+	reses := []string{}
+
+	var s scanner.Scanner
+	s.Init(strings.NewReader(string(out)))
+	var tok rune
+	for tok != scanner.EOF {
+		tok = s.Scan()
+		if s.TokenText() == "resource" {
+			tok = s.Scan()
+			if tok == scanner.EOF {
+				return nil, errors.New("drbdsetup show: scanning failure")
+			}
+			reses = append(reses, s.TokenText())
+		}
+	}
+
+	return reses, nil
+}
